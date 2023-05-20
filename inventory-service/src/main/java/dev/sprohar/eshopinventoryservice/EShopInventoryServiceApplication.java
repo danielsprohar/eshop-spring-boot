4535ac88267028b70152ec8760528a82ddea3f35
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-20 18:19:05 -0500

    Eureka: Registration and Discovery

--- inventory-service/src/main/java/dev/sprohar/eshopinventoryservice/EShopInventoryServiceApplication.java
+++ inventory-service/src/main/java/dev/sprohar/eshopinventoryservice/EShopInventoryServiceApplication.java
@@ -1,29 +1,24 @@
 package dev.sprohar.eshopinventoryservice;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import dev.sprohar.eshopinventoryservice.model.Inventory;
+import dev.sprohar.eshopinventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
-import dev.sprohar.eshopinventoryservice.model.Inventory;
-import dev.sprohar.eshopinventoryservice.repository.InventoryRepository;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EShopInventoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EShopInventoryServiceApplication.class, args);
 	}
 
-	/**
-	 * Initialize the database with some test entries
-	 * 
-	 * @param inventoryRepository
-	 * @return
-	 */
 	@Bean
 	CommandLineRunner init(InventoryRepository inventoryRepository) {
 		List<Inventory> inventory = new ArrayList<>();
@@ -32,10 +27,7 @@ public class EShopInventoryServiceApplication {
 		inventory.add(Inventory.builder().sku(3L).quantity(0).build());
 
 		return args -> {
-			inventory.stream()
-					.forEach(item -> {
-						inventoryRepository.save(item);
-					});
+			inventoryRepository.saveAll(inventory);
 		};
 	}
 
