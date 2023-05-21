365cd540978a4b11897dc8a7d98925f409d4e957
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-21 13:35:03 -0500

    Configured the API gateway to ping Keycloak

---
+++ api-gateway/src/main/java/dev/sprohar/eshopgateway/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package dev.sprohar.eshopgateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+// Spring Cloud Gateway is a Flux project
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity
+                .csrf()
+                .disable()
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers("/eureka", "/eureka/**")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated()
+                )
+                .oauth2ResourceServer(
+                        ServerHttpSecurity.OAuth2ResourceServerSpec::jwt
+                );
+
+        return serverHttpSecurity.build();
+    }
+}
