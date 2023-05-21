1a6c1fcdfc0eef9b04237fa0dd95cd5dbb3558fb
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-20 23:15:03 -0500

    API Gateway: scaffold project

---
+++ api-gateway/src/test/java/dev/sprohar/eshopgateway/EShopApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sprohar.eshopgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
