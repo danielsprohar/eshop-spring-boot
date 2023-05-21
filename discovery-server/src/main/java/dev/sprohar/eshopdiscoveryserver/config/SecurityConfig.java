38d6b160c1c1052e389e988e71dd996859be7732
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-21 15:51:00 -0500

    Discovery Service: security configuration.

---
+++ discovery-server/src/main/java/dev/sprohar/eshopdiscoveryserver/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+/**
+ * References:
+ * 1. https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
+ * 2. https://docs.spring.io/spring-security/site/docs/current/api/org/springframework/security/crypto/argon2/Argon2PasswordEncoder.html
+ */
+package dev.sprohar.eshopdiscoveryserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    /**
+     * Get the value from `application.properties`
+     */
+    @Value("${eureka.username}")
+    private String username;
+
+    /**
+     * Get the value from `application.properties`
+     */
+    @Value("${eureka.password}")
+    private String password;
+
+
+    /**
+     * In-memory authentication
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User.withUsername(this.username)
+                .password(encoder.encode(this.password))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    /**
+     * Filter chain
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorizeHttpRequests) ->
+                        authorizeHttpRequests.anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+}
--- discovery-server/src/test/java/dev/sprohar/eshopdiscoveryserver/EShopDiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/dev/sprohar/eshopdiscoveryserver/EShopDiscoveryServerApplicationTests.java
@@ -1,13 +1,43 @@
 package dev.sprohar.eshopdiscoveryserver;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class EShopDiscoveryServerApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	@WithAnonymousUser
+	void shouldReturnStatusCodeUnauthorizedWhenNoCredentialsAreGiven() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/"))
+				.andExpect(status().isUnauthorized());
 	}
 
+	/**
+	 * The <code>WithUserDetails</code> annotation uses "password" and "USER"
+	 * as the default values for <code>password</code> and <code>role</code>,
+	 * respectively.
+	 * <br /><br />
+	 * The value for <code>value</code> is the username that you defined in
+	 * <code>application.settings</code>
+	 */
+	@Test
+//	@WithMockUser(username = "eureka")
+	@WithUserDetails(value = "eureka")
+	void shouldReturnStatusCodeOkWhenValidCredentialsAreGiven() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/"))
+				.andExpect(status().isOk());
+	}
 }
